265c60c33b07519a9e0cf3fbfde11fdaf6091d19
Diego Armindo <diego.armindo@newm.com.br>
2021-08-13 00:55:21 -0300

    Finalização do auth

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create'); //deve ficar em primeiro/ rota para retornar o formulário de create
-Route::any('/tickets/search', [TicketController::class, 'search'])->name('tickets.search');
-Route::put('/tickets/{id}', [TicketController::class, 'update'])->name('tickets.update');
-Route::get('/tickets/edit/{id}', [TicketController::class, 'edit'])->name('tickets.edit');
-Route::delete('/tickets/{id}', [TicketController::class, 'destroy'])->name('tickets.destroy');
-Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets.show');
-Route::post('/tickets', [TicketController::class, 'creation'])->name('tickets.creation'); //rota para enviar o formulário
-Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index'); //rota para retornar a listagem de tickets
+//grupo de autentição
+Route::middleware(['auth'])->group(function()
+{
+    Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create'); //deve ficar em primeiro/ rota para retornar o formulário de create
+    Route::any('/tickets/search', [TicketController::class, 'search'])->name('tickets.search'); //rota para buscar o ticket
+    Route::put('/tickets/{id}', [TicketController::class, 'update'])->name('tickets.update'); //rota para fazer o update
+    Route::get('/tickets/edit/{id}', [TicketController::class, 'edit'])->name('tickets.edit'); //rota para exibir o form de edit
+    Route::delete('/tickets/{id}', [TicketController::class, 'destroy'])->name('tickets.destroy'); //rota para excluir o ticket
+    Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets.show'); //rota para exibir o ticket em modo exibição
+    Route::post('/tickets', [TicketController::class, 'creation'])->name('tickets.creation'); //rota para enviar o formulário
+    Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index'); //rota para retornar a listagem de tickets
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
